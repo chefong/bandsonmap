d1f3f0f29f25fc46f0c5fcced086d6268188d527
ericong18 <ericong18@gmail.com>
2018-08-11 23:56:40 -0700

    Displayed markers where artists are performing on the map, but going to use a different method to increase performance

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,43 +1,70 @@
 import React, { Component } from 'react';
-import ReactMapboxGl from "react-mapbox-gl";
+import ReactMapboxGl, { Marker } from "react-mapbox-gl";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faHome } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { NavLink } from 'react-router-dom';
+import Marks from './Marks';
 import './Layout.css';
 
 library.add(faSearch, faHome);
 
+const musicIcon = require('../assets/imgs/music-player.png');
 const mapBoxToken = process.env.REACT_APP_MAPBOX_TOKEN;
 const bandsintownID = process.env.REACT_APP_BANDSINTOWN_ID;
 
-const Map = ReactMapboxGl({
+const Mapbox = ReactMapboxGl({
   accessToken: mapBoxToken
 });
 
 class Layout extends Component {
 
   state = {
-    center: [-122.431297, 37.773972] // [longitude, latitude] of SF (default)
+    center: [-122.431297, 37.773972], // [longitude, latitude] of SF (default)
+    name: undefined,
+    id: undefined,
+    imageURL: undefined, 
+    events: undefined
+  }
+
+  resetState = () => {
+    this.setState({
+      name: undefined,
+      id: undefined,
+      imageURL: undefined,
+      events: undefined
+    })
   }
 
   handleSubmit = async e => {
     e.preventDefault();
 
+    this.resetState();
+
     let artistName = e.target.elements.searchbox.value;
+    let eventList = [];
 
     // Fetch artist data
     const artist_API_CALL = await fetch(`https://rest.bandsintown.com/artists/${artistName}?app_id=${bandsintownID}`);
     const artistData = await artist_API_CALL.json();
     console.log(artistData);
+    this.setState({
+      name: artistData.name,
+      id: artistData.id,
+      imageURL: artistData.image_url
+    })
 
     // Fetch artist event data
     const artistEvent_API_CALL = await fetch(`https://rest.bandsintown.com/artists/${artistName}/events?app_id=${bandsintownID}`);
     const artistEventData = await artistEvent_API_CALL.json();
     console.log(artistEventData);
+    for (let i = 0; i < artistEventData.length; ++i) {
+      eventList.push(artistEventData[i]);
+    }
+    this.setState({events: eventList});
   }
 
-  componentDidMount = () => {
+  componentWillMount = () => {
     navigator.geolocation.getCurrentPosition(location => {
       this.setState({
         center: [location.coords.longitude, location.coords.latitude]
@@ -70,14 +97,22 @@ class Layout extends Component {
           </div>
         </div>
         <div className="map-container">
-          <Map
+          <Mapbox
             style="mapbox://styles/ericong18/cjkhgo9ti2o5z2so5c0s0gng8"
             center={this.state.center}
             containerStyle={{
               height: "100vh",
               width: "100vw"
             }}
-          />
+          >
+            { this.state.events && this.state.events.map(artistEvent => {
+              return (
+                <Marker coordinates={[artistEvent.venue.longitude, artistEvent.venue.latitude]} key={artistEvent.id}>
+                  <img src={ musicIcon } alt=""/>
+                </Marker>
+              )}) 
+            }
+          </Mapbox>
         </div>
       </div>
     );
---
+++ src/components/Marks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Marker } from 'react-map-gl';
+import './Marks.css';
+
+const musicIcon = require('../assets/imgs/music-player.png');
+
+const Marks = props => {
+  { console.log(props.artistEvents) }
+  return (null)
+}
+
+export default Marks;
